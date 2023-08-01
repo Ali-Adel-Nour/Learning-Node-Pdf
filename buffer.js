f9d8b828a839f4f7c6de8b4de37d806610e90e85
Ali-Adel-Nour <aliiiadel841@gmail.com>
2023-08-01 11:48:47 +0300

    event emitt examples

--- buffer.js
+++ buffer.js
@@ -9,7 +9,7 @@ console.log(b3);
 let b4 = Buffer.allocUnsafe(10);
 console.log(b4);
 
-//Depracted
+
 let buf = Buffer.from('This is my pretty example');
 let json = JSON.stringify({ data: [...buf] });
 let buf2 = Buffer.from(JSON.parse(json).data);
